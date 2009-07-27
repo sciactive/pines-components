97e902acdcae0377de08f21a6e1c7e071cdfade9
Hunter Perrin <hperrin@gmail.com>
2009-07-27 04:13:06 +0000

    Fixed bug in com_about requiring ability manager or error.

--- com_about/common.php
+++ com_about/common.php
@@ -1,6 +1,8 @@
 <?php
 defined('P_RUN') or die('Direct access prohibited');
 
-$config->ability_manager->add('com_about', 'show', 'About Page', 'User can see the about page.');
+if ( isset($config->ability_manager) ) {
+	$config->ability_manager->add('com_about', 'show', 'About Page', 'User can see the about page.');
+}
 
 ?>

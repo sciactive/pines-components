7082d345848d69319a5c52a8176725a5727a0aca
Hunter Perrin <hperrin@gmail.com>
2009-07-28 22:11:15 +0000

    Fully documented com_mailer.

--- com_mailer/configure.php
+++ com_mailer/configure.php
@@ -22,13 +22,57 @@
  * @subpackage com_mailer
  */
 class com_mailer extends component {
+    /**
+     * The sender's email address.
+     *
+     * @var string
+     */
 	var $sender;
+    /**
+     * The recipient's email address.
+     *
+     * @var string
+     */
 	var $recipient;
+    /**
+     * The message subject.
+     *
+     * @var string
+     */
 	var $subject;
-	var $headers=array();
-	var $mimeTypes=array();
-	var $attachments=array();
-	
+    /**
+     * The message text's MIME type.
+     *
+     * @var string
+     */
+	var $text_mime_type = 'text/html';
+    /**
+     * An array of headers to include in the message.
+     *
+     * @var array
+     */
+	var $headers = array();
+    /**
+     * An array of known MIME types.
+     *
+     * The values are the mime types, and the keys are the file extensions.
+     *
+     * @var array
+     */
+	var $mimeTypes = array();
+    /**
+     * An array of attachments.
+     *
+     * @var array
+     */
+	var $attachments = array();
+
+    /**
+     * @param string $sender The sender's email address.
+     * @param string $recipient The recipient's email address.
+     * @param string $subject The message subject.
+     * @param string $message The message text.
+     */
     function __construct($sender, $recipient, $subject, $message) {
 		// validate incoming parameters
 		if (!preg_match("/^.+@.+$/",$sender)) {
@@ -57,21 +101,59 @@ class com_mailer extends component {
 		$this->headers['X-Sender']=$this->sender;
 		$this->headers['X-Priority']='3';
 		// define some default MIME types
-		$this->mimeTypes['image/jpeg']='jpg';
-		$this->mimeTypes['image/jpg']='jpg';
-		$this->mimeTypes['image/gif']='gif';
-		$this->mimeTypes['text/plain']='txt';
-		$this->mimeTypes['text/html']='htm';
-		$this->mimeTypes['text/xml']='xml';
-		$this->mimeTypes['application/pdf']='pdf';
+        $this->mimeTypes['doc']='application/msword';
+		$this->mimeTypes['pdf']='application/pdf';
+        $this->mimeTypes['gz']='application/x-gzip';
+        $this->mimeTypes['exe']='application/x-msdos-program';
+        $this->mimeTypes['rar']='application/x-rar-compressed';
+        $this->mimeTypes['swf']='application/x-shockwave-flash';
+        $this->mimeTypes['tgz']='application/x-tar-gz';
+        $this->mimeTypes['tar']='application/x-tar';
+        $this->mimeTypes['zip']='application/zip';
+        $this->mimeTypes['mid']='audio/midi';
+        $this->mimeTypes['mp3']='audio/mpeg';
+        $this->mimeTypes['au']='audio/ulaw';
+        $this->mimeTypes['aif']='audio/x-aiff';
+        $this->mimeTypes['aiff']='audio/x-aiff';
+        $this->mimeTypes['wma']='audio/x-ms-wma';
+        $this->mimeTypes['wav']='audio/x-wav';
+		$this->mimeTypes['gif']='image/gif';
+		$this->mimeTypes['jpg']='image/jpeg';
+		$this->mimeTypes['jpeg']='image/jpeg';
+		$this->mimeTypes['jpe']='image/jpeg';
+        $this->mimeTypes['png']='image/png';
+        $this->mimeTypes['tif']='image/tiff';
+        $this->mimeTypes['tiff']='image/tiff';
+        $this->mimeTypes['css']='text/css';
+		$this->mimeTypes['htm']='text/html';
+		$this->mimeTypes['html']='text/html';
+		$this->mimeTypes['txt']='text/plain';
+        $this->mimeTypes['rtf']='text/rtf';
+		$this->mimeTypes['xml']='text/xml';
+        $this->mimeTypes['flv']='video/flv';
+        $this->mimeTypes['mpe']='video/mpeg';
+        $this->mimeTypes['mpeg']='video/mpeg';
+        $this->mimeTypes['mpg']='video/mpeg';
+        $this->mimeTypes['mov']='video/quicktime';
+        $this->mimeTypes['asf']='video/x-ms-asf';
+        $this->mimeTypes['wmv']='video/x-ms-wmv';
+        $this->mimeTypes['avi']='video/x-msvideo';
 	}
 
-	// create text part of the message
+	/**
+     * Create text part of the message.
+     *
+     * @return string The text.
+     */
 	function buildTextPart() {
-		return "--MIME_BOUNDRY\nContent-Type: text/html; charset=iso-8859-1\nContent-Transfer-Encoding: 7bit\n\n\n".$this->message."\n\n";
+		return "--MIME_BOUNDRY\nContent-Type: ".$this->text_mime_type."; charset=iso-8859-1\nContent-Transfer-Encoding: 7bit\n\n\n".$this->message."\n\n";
 	}
 
-	// create attachments part of the message
+	/**
+     * Create attachments part of the message.
+     *
+     * @return string The attachment section.
+     */
 	function buildAttachmentPart() {
 		if (count($this->attachments) > 0) {
 			$attachmentPart='';
@@ -84,7 +166,11 @@ class com_mailer extends component {
 		}
 	}
 
-	// create message MIME headers
+	/**
+     * Create message MIME headers.
+     *
+     * @return string The MIME headers.
+     */
 	function buildHeaders() {
 		foreach($this->headers as $name=>$value) {
 			$headers[]=$name.': '.$value;
@@ -92,29 +178,51 @@ class com_mailer extends component {
 		return implode("\n",$headers)."\nThis is a multi-part message in MIME format.\n";
 	}
 
-	// add new MIME header
-	function addHeader($name,$value) {
+    /**
+     * Add new MIME header.
+     *
+     * @param string $name The header's name.
+     * @param string $value The header's value.
+     */
+	function addHeader($name, $value) {
 		$this->headers[$name]=$value;
 	}
 
-	// add new attachment
+    /**
+     * Add new attachment.
+     *
+     * @param string $attachment The attachment filename.
+     * @return bool True on success, false on failure.
+     */
 	function addAttachment($attachment) {
 		if (!file_exists($attachment)) {
 			display_error('Invalid attachment.');
+            return false;
 		}
-		$this->attachments[]=$attachment;
+        $this->attachments[]=$attachment;
+        return true;
 	}
 
-	// get MIME Type of attachment
+    /**
+     * Get MIME Type of attachment.
+     *
+     * @param string $attachment The attachment filename.
+     * @return mixed MIME type on success, null on failure.
+     */
 	function getMimeType($attachment) {
 		$attachment=explode('.',basename($attachment));
-		if (!$mimeType=array_search(strtolower($attachment[count($attachment)-1]),$this->mimeTypes)) {
+		if (!isset($this->mimeTypes[strtolower($attachment[count($attachment)-1])])) {
 			display_error('MIME Type not found.');
+            return null;
 		}
-		return $mimeType;
+		return $this->mimeTypes[strtolower($attachment[count($attachment)-1])];
 	}
 
-	// send email
+	/**
+     * Send email.
+     *
+     * @return bool True on success, false on failure.
+     */
 	function send() {
 		$to=$this->recipient;
 		$subject=$this->subject;

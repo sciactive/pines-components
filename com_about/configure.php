eb930a04a45e8848c95066ec3fe6f307492a2c82
Hunter Perrin <hperrin@gmail.com>
2009-07-28 22:40:52 +0000

    Fully documented com_about.

--- com_about/configure.php
+++ com_about/configure.php
@@ -13,10 +13,14 @@ defined('P_RUN') or die('Direct access prohibited');
 
 $config->com_about = new DynamicConfig;
 
-// Description of your installation.
+/**
+ * Description of your installation.
+ */
 $config->com_about->description = "This is the default installation for ".$config->option_title.".";
 
-// Whether to show Pines's description underneath yours.
+/**
+ * Whether to show Pines' description underneath yours.
+ */
 $config->com_about->describe_self = true;
 
 ?>

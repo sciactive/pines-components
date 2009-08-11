53ad5cb2cdd31ea22da71539cc238dcb5ad5d56f
Hunter Perrin <hperrin@gmail.com>
2009-08-11 21:42:46 +0000

    Actions for com_logger.

---
+++ com_logger/actions/view.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Display the log.
+ *
+ * @package Pines
+ * @subpackage com_logger
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html
+ * @author Hunter Perrin <hunter@sciactive.com>
+ * @copyright Hunter Perrin
+ * @link http://sciactive.com/
+ */
+defined('P_RUN') or die('Direct access prohibited');
+
+display_notice('Not implemented yet.');
+
+?>

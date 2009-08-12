70ea61eea87def40d0369db0418a49249c47812b
Hunter Perrin <hperrin@gmail.com>
2009-08-12 00:36:50 +0000

    Fixed some documentation problems.

--- com_logger/load.php
+++ com_logger/load.php
@@ -14,6 +14,12 @@ defined('P_RUN') or die('Direct access prohibited');
 $config->log_manager = new com_logger;
 
 if (!function_exists('display_error') && $config->com_logger->log_errors) {
+    /**
+     * Log a displayed error.
+     *
+     * @param string $error_text The error text.
+     * @ignore
+     */
 	function display_error($error_text) {
 		global $config, $page;
         $config->log_manager->log($error_text, 'error');
@@ -22,6 +28,12 @@ if (!function_exists('display_error') && $config->com_logger->log_errors) {
 }
 
 if (!function_exists('display_notice') && $config->com_logger->log_notices) {
+    /**
+     * Log a displayed notice.
+     *
+     * @param string $notice_text The notice text.
+     * @ignore
+     */
 	function display_notice($notice_text) {
 		global $config, $page;
         $config->log_manager->log($notice_text, 'notice');

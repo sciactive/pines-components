7b07c776aee454d40255430747290fb54ecd05dd
Hunter Perrin <hperrin@gmail.com>
2009-10-19 17:06:10 +0000

    Added a default action for com_newsletter.

---
+++ com_newsletter/actions/default.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Load a list of mails.
+ *
+ * @package Pines
+ * @subpackage com_newsletter
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html
+ * @author Hunter Perrin <hunter@sciactive.com>
+ * @copyright Hunter Perrin
+ * @link http://sciactive.com/
+ */
+defined('P_RUN') or die('Direct access prohibited');
+
+action('com_newsletter', 'list');
+
+?>

98c65b48c74dd46deea25b2dfaffbd26ea5b4aab
Hunter Perrin <hperrin@gmail.com>
2009-11-25 00:35:27 +0000

    Work on Pines Notify and receiving POs.

--- com_sales/views/xhtml-1.0/form_receive.php
+++ com_sales/views/xhtml-1.0/form_receive.php
@@ -36,41 +36,6 @@ $this->note = 'Only use this form to receive inventory into your <strong>current
 				pgrid_toolbar_contents : [
 					{
 						type: 'button',
-						text: 'Add',
-						extra_class: 'icon picon_16x16_actions_document-new',
-						selection_optional: true,
-						click: function(){
-							if (!cur_vendor) {
-								$("<div title=\"Alert\">Please select a vendor.</div>").dialog({
-									bgiframe: true,
-									modal: true,
-									buttons: {
-										Ok: function(){
-											$(this).dialog("close");
-										}
-									}
-								});
-								return;
-							}
-							product_dialog.dialog('open');
-						}
-					},
-					{
-						type: 'button',
-						text: 'Edit',
-						extra_class: 'icon picon_16x16_actions_document-open',
-						double_click: true,
-						click: function(e, rows){
-							var row_data = products_table.pgrid_export_rows(rows);
-							available_products_table.pgrid_select_rows([row_data[0].key]);
-							$("#cur_product_quantity").val(row_data[0].values[2]);
-							$("#cur_product_cost").val(row_data[0].values[3]);
-							product_dialog.dialog('open');
-							rows.pgrid_delete();
-						}
-					},
-					{
-						type: 'button',
 						text: 'Remove',
 						extra_class: 'icon picon_16x16_actions_edit-delete',
 						click: function(e, rows){
@@ -86,26 +51,12 @@ $this->note = 'Only use this form to receive inventory into your <strong>current
 				bgiframe: true,
 				autoOpen: false,
 				modal: true,
-				width: 600,
 				buttons: {
 					"Done": function() {
-						var cur_product_quantity = parseInt($("#cur_product_quantity").val());
-						var cur_product_cost = parseFloat($("#cur_product_cost").val());
-						var cur_product = available_products_table.pgrid_get_selected_rows().pgrid_export_rows();
-						if (!cur_product[0]) {
-							$("<div title=\"Alert\">Please select a product.</div>").dialog({
-								bgiframe: true,
-								modal: true,
-								buttons: {
-									Ok: function(){
-										$(this).dialog("close");
-									}
-								}
-							});
-							return;
-						}
-						if (isNaN(cur_product_quantity) || isNaN(cur_product_cost)) {
-							$("<div title=\"Alert\">Please provide both a quantity and a cost for this product.</div>").dialog({
+						var cur_serial = $("#cur_serial").val();
+						var cur_product = $("#upc").val();
+						if (!cur_serial) {
+							$("<div title=\"Alert\">Please provide a serial number.</div>").dialog({
 								bgiframe: true,
 								modal: true,
 								buttons: {
@@ -117,16 +68,14 @@ $this->note = 'Only use this form to receive inventory into your <strong>current
 							return;
 						}
 						var new_product = [{
-							key: cur_product[0].key,
+							key: cur_serial,
 							values: [
-								cur_product[0].values[0],
-								cur_product[0].values[1],
-								cur_product_quantity,
-								cur_product_cost,
-								round_to_dec(cur_product_quantity * cur_product_cost)
+								cur_product,
+								cur_serial
 							]
 						}];
 						products_table.pgrid_add(new_product);
+						$("#upc").val("");
 						$(this).dialog('close');
 					}
 				},
@@ -135,34 +84,78 @@ $this->note = 'Only use this form to receive inventory into your <strong>current
 				}
 			});
 
+			$("#add_product").click(function(){
+				if (!$("#upc").val()) {
+					$("<div title=\"Alert\">Please enter a UPC to add a product.</div>").dialog({
+						bgiframe: true,
+						modal: true,
+						buttons: {
+							Ok: function(){
+								$(this).dialog("close");
+							}
+						}
+					});
+					return;
+				}
+				product_dialog.dialog('open');
+			});
+
 			products_table.pgrid_get_all_rows().pgrid_delete();
 			update_products();
+			$("#receive_tabs").tabs();
 		});
 		// ]]>
 	</script>
-	<div class="element">
-		<label><span class="label">UPC</span>
-		<input class="field" type="text" name="upc" size="20" /></label>
-	</div>
-	<div class="element full_width">
-		<span class="label">Products</span>
-		<div class="group">
-			<div class="field">
-				<table id="products_table">
-					<thead>
-						<tr>
-							<th>UPC</th>
-							<th>Serial</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr><td>-----------</td><td>-----------</td></tr>
-					</tbody>
-				</table>
+	<div id="receive_tabs" style="clear: both;">
+		<ul>
+			<li><a href="#tab_standard">Standard Entry</a></li>
+			<li><a href="#tab_bulk">Bulk Entry</a></li>
+		</ul>
+		<div id="tab_standard">
+			<div class="element">
+				<label><span class="label">UPC</span>
+				<input class="field" type="text" id="upc" name="upc" size="20" /></label>
+				<input class="button ui-state-default ui-corner-all" type="button" id="add_product" value="Add" />
 			</div>
-			<input class="field" type="hidden" id="products" name="products" size="20" />
+			<div class="element full_width">
+				<span class="label">Products</span>
+				<div class="group">
+					<div class="field">
+						<table id="products_table">
+							<thead>
+								<tr>
+									<th>UPC</th>
+									<th>Serial</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr><td>-----------</td><td>-----------</td></tr>
+							</tbody>
+						</table>
+					</div>
+					<input class="field" type="hidden" id="products" name="products" size="20" />
+				</div>
+			</div>
+			<div id="product_dialog" title="Receive Inventory">
+				<label>
+					<span>Serial #</span>
+					<input type="text" name="cur_serial" id="cur_serial" />
+				</label>
+			</div>
+			<br class="spacer" />
+		</div>
+		<div id="tab_bulk">
+			<div class="element full_width">
+				<span class="label">UPC/Serial Combos</span>
+				<span class="note">Enter one UPC, a new line, the corresponding serial, then a new line, and repeat.</span>
+				<div class="group">
+					<textarea rows="8" cols="35" style="width: 100%;" name="upc_bulk"></textarea>
+				</div>
+			</div>
+			<br class="spacer" />
 		</div>
 	</div>
+	<br />
 	<div class="element buttons">
 		<input class="button ui-state-default ui-priority-primary ui-corner-all" type="submit" value="Submit" />
 		<input class="button ui-state-default ui-priority-secondary ui-corner-all" type="button" onclick="window.location='<?php echo pines_url(); ?>';" value="Cancel" />

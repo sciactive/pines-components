2800f5d3a46f06fa47384dac3bcc7c3ede80b309
Hunter Perrin <hperrin@gmail.com>
2009-11-25 19:44:25 +0000

    Took over alert().

--- pines/js/template.js
+++ pines/js/template.js
@@ -1,3 +1,5 @@
+var _alert;
+
 function convert_standard_notices() {
 	// Turn notices into Pines Notify notices.
 	$(".notice").find("p.entry span.text").each(function(){
@@ -15,10 +17,28 @@ function convert_standard_notices() {
 	}).end().remove();
 }
 
+function consume_alert() {
+	if (_alert) return;
+	_alert = window.alert;
+	window.alert = function(message) {
+		$.pnotify({
+			pnotify_title: 'Alert',
+			pnotify_text: message
+		});
+	};
+}
+
+function release_alert() {
+	if (!_alert) return;
+	window.alert = _alert;
+	_alert = null;
+}
+
 jQuery(document).ready(function($){
 	// Just in case Pines Notify isn't working.
 	$(".notice .close, .error .close").css("cursor", "pointer").click(function() {
 		$(this).parent().fadeOut("slow");
 	});
 	convert_standard_notices();
+	consume_alert();
 });

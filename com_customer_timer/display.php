631da405e3766ede9b9d9648467d79644e01399c
Hunter Perrin <hperrin@gmail.com>
2009-12-19 23:27:45 +0000

    Beginning com_customer_timer. This will time customers, deducting points per minute.

---
+++ com_customer_timer/display.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * com_customer_timer's display control.
+ *
+ * @package Pines
+ * @subpackage com_customer_timer
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html
+ * @author Hunter Perrin <hunter@sciactive.com>
+ * @copyright Hunter Perrin
+ * @link http://sciactive.com/
+ */
+defined('P_RUN') or die('Direct access prohibited');
+
+if ( gatekeeper('com_customer_timer/status') || gatekeeper('com_customer_timer/login') ) {
+	$com_customer_timer_menu_id = $page->main_menu->add('Customer Timer');
+	if ( gatekeeper('com_customer_timer/status') )
+		$page->main_menu->add('Status', pines_url('com_customer_timer', 'status'), $com_customer_timer_menu_id);
+	if ( gatekeeper('com_customer_timer/login') )
+		$page->main_menu->add('Login', pines_url('com_customer_timer', 'login'), $com_customer_timer_menu_id);
+}
+
+?>

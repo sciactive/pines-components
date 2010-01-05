7f9c42584ad9f450d68e44b76a08cb7f948e866c
Hunter Perrin <hperrin@gmail.com>
2010-01-05 22:00:57 +0000

    Formatting.

--- com_user/actions/exit.php
+++ com_user/actions/exit.php
@@ -14,18 +14,18 @@ defined('P_RUN') or die('Direct access prohibited');
 display_notice($_REQUEST['message']);
 if (is_object($_SESSION['user'])) {
 // Load the user's default component.
-	if ($_REQUEST['default'] == '1') {
-	$module = new module('com_user', 'default_denied', 'content');
-	display_error('Incorrect user permission settings detected.');
-	} else {
-	if ( !empty($_REQUEST['url']) ) {
-		$module = new module('com_user', 'punted', 'right');
-		$module->url = urldecode($_REQUEST['url']);
-	}
-	action($config->default_component, 'default');
-	}
+    if ($_REQUEST['default'] == '1') {
+        $module = new module('com_user', 'default_denied', 'content');
+        display_error('Incorrect user permission settings detected.');
+    } else {
+        if ( !empty($_REQUEST['url']) ) {
+            $module = new module('com_user', 'punted', 'right');
+            $module->url = urldecode($_REQUEST['url']);
+        }
+        action($config->default_component, 'default');
+    }
 } else {
 // Load the default component.
-	action($config->default_component, 'default');
+    action($config->default_component, 'default');
 }
 ?>

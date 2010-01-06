e1132ccd01e98a614b49fbd10e14339dd0e41e1d
Hunter Perrin <hperrin@gmail.com>
2010-01-06 00:12:39 +0000

    PO and vendor class expanded.

--- com_sales/classes/com_sales_po.php
+++ com_sales/classes/com_sales_po.php
@@ -18,11 +18,30 @@ defined('P_RUN') or die('Direct access prohibited');
  * @subpackage com_sales
  */
 class com_sales_po extends entity {
-	public function __construct() {
+	/**
+	 * Load a PO.
+	 * @param int $id The ID of the PO to load, null for a new PO.
+	 */
+	public function __construct($id = null) {
 		parent::__construct();
 		$this->add_tag('com_sales', 'po');
+		if (!is_null($id)) {
+			global $config;
+			$entity = $config->entity_manager->get_entity($id, $this->tags, get_class($this));
+			if (is_null($entity))
+				return;
+			$this->guid = $entity->guid;
+			$this->parent = $entity->parent;
+			$this->tags = $entity->tags;
+			$this->entity_cache = array();
+			$this->put_data($entity->get_data());
+		}
 	}
 
+	/**
+	 * Delete the PO.
+	 * @return bool True on success, false on failure.
+	 */
 	public function delete() {
 		// Don't delete the PO if it has received items.
 		if (!empty($this->received))
@@ -33,10 +52,41 @@ class com_sales_po extends entity {
 		return true;
 	}
 
+	/**
+	 * Save the PO.
+	 * @return bool True on success, false on failure.
+	 */
 	public function save() {
 		if (!isset($this->po_number))
 			return false;
 		return parent::save();
+	}
+
+	/**
+	 * Print a form to edit the PO.
+	 * @return module The form's module.
+	 */
+	public function print_form() {
+		global $config;
+		$pgrid = new module('system', 'pgrid.default', 'head');
+		$pgrid->icons = true;
+		$module = new module('com_sales', 'form_po', 'content');
+		$module->entity = $this;
+		$module->locations = $config->user_manager->get_group_array();
+		$module->shippers = $config->entity_manager->get_entities_by_tags('com_sales', 'shipper', com_sales_shipper);
+		if (!is_array($module->shippers)) {
+			$module->shippers = array();
+		}
+		$module->vendors = $config->entity_manager->get_entities_by_tags('com_sales', 'vendor', com_sales_vendor);
+		if (!is_array($module->vendors)) {
+			$module->vendors = array();
+		}
+		$module->products = $config->entity_manager->get_entities_by_tags('com_sales', 'product', com_sales_product);
+		if (!is_array($module->products)) {
+			$module->products = array();
+		}
+
+		return $module;
 	}
 }
 
--- com_sales/classes/com_sales_vendor.php
+++ com_sales/classes/com_sales_vendor.php
@@ -18,11 +18,30 @@ defined('P_RUN') or die('Direct access prohibited');
  * @subpackage com_sales
  */
 class com_sales_vendor extends entity {
-	public function __construct() {
+	/**
+	 * Load a vendor.
+	 * @param int $id The ID of the vendor to load, null for a new vendor.
+	 */
+	public function __construct($id = null) {
 		parent::__construct();
 		$this->add_tag('com_sales', 'vendor');
+		if (!is_null($id)) {
+			global $config;
+			$entity = $config->entity_manager->get_entity($id, $this->tags, get_class($this));
+			if (is_null($entity))
+				return;
+			$this->guid = $entity->guid;
+			$this->parent = $entity->parent;
+			$this->tags = $entity->tags;
+			$this->entity_cache = array();
+			$this->put_data($entity->get_data());
+		}
 	}
 
+	/**
+	 * Delete the vendor.
+	 * @return bool True on success, false on failure.
+	 */
 	public function delete() {
 		if (!parent::delete())
 			return false;
@@ -30,10 +49,25 @@ class com_sales_vendor extends entity {
 		return true;
 	}
 
+	/**
+	 * Save the vendor.
+	 * @return bool True on success, false on failure.
+	 */
 	public function save() {
 		if (!isset($this->name))
 			return false;
 		return parent::save();
+	}
+
+	/**
+	 * Print a form to edit the vendor.
+	 * @return module The form's module.
+	 */
+	public function print_form() {
+		$module = new module('com_sales', 'form_vendor', 'content');
+		$module->entity = $this;
+
+		return $module;
 	}
 }
 

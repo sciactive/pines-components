a3f57e88f4d82cf5a6f9ae1493f7dcf9e78180a8
Zak Huber <zak@sciactive.com>
2010-01-19 20:55:51 +0000

    Authorize.Net component work.

--- com_authorizenet/classes/com_authorizenet.php
+++ com_authorizenet/classes/com_authorizenet.php
@@ -40,15 +40,111 @@ class com_authorizenet extends component {
 				$page->override_doc($module->render());
 				break;
 			case 'approve':
-				$args['payment']['status'] = 'pending';
-				//$args['payment']['data']['name_first'];
-				//$args['payment']['status'] = 'declined';
-				//$args['payment']['status'] = 'approved';
+				$args['payment']['status'] = 'approved';
+				if (empty($args['payment']['data']['name_first']) ||
+					empty($args['payment']['data']['name_last']) ||
+					empty($args['payment']['data']['address']) ||
+					empty($args['payment']['data']['state']) ||
+					empty($args['payment']['data']['zip']) ||
+					empty($args['payment']['data']['card_number']) ||
+					empty($args['payment']['data']['card_exp_month']) ||
+					empty($args['payment']['data']['card_exp_year']) ||
+					empty($args['payment']['data']['cid']))
+					$args['payment']['status'] = 'info_requested';
 				break;
 			case 'tender':
-				$args['payment']['status'] = 'pending';
-				//$args['payment']['status'] = 'declined';
-				//$args['payment']['status'] = 'tendered';
+				$firstname = $args['payment']['data']['name_first'];
+				$lastname = $args['payment']['data']['name_last'];
+				$amt = (float) $args['payment']['amount'];
+				$card_num = $args['payment']['data']['card_number'];
+				$exp_date = $args['payment']['data']['card_exp_month'].$args['payment']['data']['card_exp_year'];
+				$address = $args['payment']['data']['address'];
+				$state = $args['payment']['data']['state'];
+				$zip = $args['payment']['data']['zip'];
+
+				$post_values = array(
+					// the API Login ID and Transaction Key must be replaced with valid values
+					'x_login'			=> $config->com_authorizenet->apilogin,
+					'x_tran_key'		=> $config->com_authorizenet->tran_key,
+					'x_test_request'	=> ($config->com_authorizenet->test_mode) ? 'TRUE' : 'FALSE',
+
+					'x_version'			=> '3.1',
+					'x_delim_data'		=> 'TRUE',
+					'x_delim_char'		=> '|',
+					'x_relay_response'	=> 'FALSE',
+
+					'x_type'			=> 'AUTH_CAPTURE',
+					'x_method'			=> 'CC',
+					'x_card_num'		=> $card_num,
+					'x_exp_date'		=> $exp_date,
+
+					'x_amount'			=> $amt,
+					'x_description'		=> $transaction_name,
+
+					'x_first_name'		=> $firstname,
+					'x_last_name'		=> $lastname,
+					'x_address'			=> $address,
+					'x_state'			=> $state,
+					'x_zip'				=> $zip
+				);
+				$post_string = "";
+				foreach ($post_values as $key => $value) {
+					$post_string .= "$key=" . urlencode($value) . "&";
+				}
+				$post_string = rtrim($post_string, "& ");
+
+				$request = curl_init($config->com_authorizenet->post_url);
+				curl_setopt($request, CURLOPT_HEADER, 0);
+				curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($request, CURLOPT_POSTFIELDS, $post_string);
+				curl_setopt($request, CURLOPT_SSL_VERIFYPEER, FALSE);
+				$post_response = curl_exec($request);
+				curl_close($request);
+
+				if ($post_response === false) {
+					display_error('Credit processing gateway cannot be reached. Please try again, and if the problem persists, please contact an administrator.');
+					break;
+				}
+
+				$response_array = explode($post_values["x_delim_char"],$post_response);
+				$args['payment']['com_authorize_credit_info'] = array_merge((array) $args['payment']['data']);
+				switch ($response_array[0]) {
+					case 1:
+						$args['payment']['status'] = 'tendered';
+						unset($args['payment']['data']['name_first']);
+						unset($args['payment']['data']['name_last']);
+						unset($args['payment']['data']['address']);
+						unset($args['payment']['data']['state']);
+						unset($args['payment']['data']['zip']);
+						unset($args['payment']['data']['card_number']);
+						unset($args['payment']['data']['card_exp_month']);
+						unset($args['payment']['data']['card_exp_year']);
+						unset($args['payment']['data']['cid']);
+						break;
+					case 2:
+						$args['payment']['status'] = 'declined';
+						unset($args['payment']['data']['name_first']);
+						unset($args['payment']['data']['name_last']);
+						unset($args['payment']['data']['address']);
+						unset($args['payment']['data']['state']);
+						unset($args['payment']['data']['zip']);
+						unset($args['payment']['data']['card_number']);
+						unset($args['payment']['data']['card_exp_month']);
+						unset($args['payment']['data']['card_exp_year']);
+						unset($args['payment']['data']['cid']);
+						break;
+					case 3:
+						$args['payment']['status'] = 'info_requested';
+						break;
+					case 4:
+						display_notice('Payment is held for review.');
+						break;
+					default:
+						$args['payment']['status'] = 'pending';
+						display_error('Credit processing failed. Please try again, and if the problem persists, please contact an administrator.');
+						break;
+				}
+				display_notice($response_array[3]);
 				break;
 		}
 	}

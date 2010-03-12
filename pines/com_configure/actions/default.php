4a766347fea8f915f26522c68f179926448f7dbc
Hunter Perrin <hperrin@gmail.com>
2010-03-12 21:24:22 +0000

    Fixed default action.

--- pines/com_configure/actions/default.php
+++ pines/com_configure/actions/default.php
@@ -14,8 +14,7 @@ defined('P_RUN') or die('Direct access prohibited');
 if ( !gatekeeper('com_configure/edit') )
 	punt_user('You don\'t have necessary permission.', pines_url('com_configure', 'default', false));
 
-$module = new module('com_configure', 'edit', 'content');
-$module->req_component = 'system';
-$module->config = $pines->configurator->get_config_array('system');
+$component = com_configure_component::factory('system');
+$component->print_form();
 
 ?>

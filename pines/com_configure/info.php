4cecfd4856be224016ea4a12e6f1eee7f0d738ed
Hunter Perrin <hperrin@gmail.com>
2010-03-28 02:30:16 +0000

    New info files.

---
+++ pines/com_configure/info.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * com_about's information.
+ *
+ * @package Pines
+ * @subpackage com_configure
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html
+ * @author Hunter Perrin <hunter@sciactive.com>
+ * @copyright Hunter Perrin
+ * @link http://sciactive.com/
+ */
+defined('P_RUN') or die('Direct access prohibited');
+
+return array(
+	'name' => 'System Configurator',
+	'author' => 'SciActive',
+	'version' => '1.0.0',
+	'license' => 'http://www.gnu.org/licenses/agpl-3.0.html',
+	'short_description' => 'Manages System Configuration',
+	'description' => 'Allows you to edit your system\'s configuration and the configuration of any installed components.',
+);
+
+?>

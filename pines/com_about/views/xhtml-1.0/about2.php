4d18877faa9c21c171dc67c251c31e0936e6d285
Hunter Perrin <hperrin@gmail.com>
2010-03-30 17:42:05 +0000

    New about page text.

--- pines/com_about/views/xhtml-1.0/about2.php
+++ pines/com_about/views/xhtml-1.0/about2.php
@@ -12,24 +12,48 @@
 defined('P_RUN') or die('Direct access prohibited');
 $this->title = "About {$pines->info->name}";
 ?>
+<style type="text/css">
+	/* <![CDATA[ */
+	#com_about_buttons {
+		float: right;
+	}
+	/* ]]> */
+</style>
+<script type="text/javascript">
+	// <![CDATA[
+	$(function(){
+		if (!$.fn.buttonset)
+			return;
+		$("#com_about_buttons").buttonset().children("button").button();
+	});
+	// ]]>
+</script>
 <p><strong>Version <?php echo $pines->info->version; ?></strong></p>
 <p>
-<?php echo $pines->info->name; ?> is a <a href="http://sciactive.com/">
-SciActive</a> project written by Hunter Perrin. It is a PHP application
-framework, designed to be an extensible MVC based framework. It allows easy
-development, easy implementation, easy maintenance, and extreme flexibility. The
-manager drops in components to add the functionality he desires. For example, if
-the manager wants to have a user management system, he simply drops in com_user.
-When com_user takes over user management for the system, it will prompt users to
-log in and only give them permissions they have been allowed.
+<?php echo $pines->info->name; ?> is a PHP application framework, designed to be
+extensible and easy to manage. It is a
+<a href="http://sciactive.com/" onclick="window.open(this.href); return false;">
+SciActive</a> project written by Hunter Perrin. It allows rapid development,
+highly customizable implementation, easy maintenance, and extreme flexibility.
+The manager installs components to add functionality. For example, if the
+manager wants to have a user management system, he can install com_user. When
+com_user takes over user management for the system, it will prompt users to log
+in and only give them permissions they have been allowed.
 </p>
 <p>
-The admin can add functions using the premade components, or write his own
-components to provide additional functionality to the system. The system will
-have a dependency verifier, which will inform the admin if he is missing
-required components and where to get them. <?php echo $pines->info->name; ?>
-was designed to allow maximum flexibility for the developer, while still
-providing a large enough base product to make development easy. The admin can
-choose whatever database environment he uses, even flat files, and thanks to the
-database abstraction layer, all the components will still work.
+The admin can add features using components, and change the look and feel using
+templates. The system will have a package manager, which will automatically
+install any required components. <?php echo $pines->info->name; ?> is designed
+to allow maximum flexibility for the developer, and provide enough tools and
+available libraries to make development of even very complex systems easy. The
+admin can choose whatever database system he uses, even flat files, and all the
+components which use an entity manager (the database abstraction layer) will
+work.
 </p>
+<p id="com_about_buttons">
+	<button type="button" onclick="window.open('http://sciactive.com/');">Visit SciActive</button>
+	<button type="button" onclick="window.open('http://sourceforge.net/projects/pines/');">Visit Project Page</button>
+	<button type="button" onclick="window.open('http://pines.sourceforge.net/pines-docs/');">Visit API Documentation</button>
+	<button type="button" onclick="window.open('http://sourceforge.net/donate/index.php?group_id=264165');">Donate</button>
+</p>
+<br class="spacer" />

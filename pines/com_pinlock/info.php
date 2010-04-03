d3ac332818d8accec3cbe190ec07211ef189a9c9
Hunter Perrin <hperrin@gmail.com>
2010-04-02 17:48:59 -0700

    New component for locking actions with a PIN.

---
+++ pines/com_pinlock/info.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * com_pinlock's information.
+ *
+ * @package Pines
+ * @subpackage com_pinlock
+ * @license http://www.gnu.org/licenses/agpl-3.0.html
+ * @author Hunter Perrin <hunter@sciactive.com>
+ * @copyright Hunter Perrin
+ * @link http://sciactive.com/
+ */
+defined('P_RUN') or die('Direct access prohibited');
+
+return array(
+	'name' => 'PIN Locker',
+	'author' => 'SciActive',
+	'version' => '1.0.0',
+	'license' => 'http://www.gnu.org/licenses/agpl-3.0.html',
+	'short_description' => 'PIN based security',
+	'description' => 'Provides a PIN based security measure to both prevent unauthorized use of accounts and securely allow users to switch accounts quickly.',
+);
+
+?>

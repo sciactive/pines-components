a4d32680b87042d6db7adde5c8e43eb4be880886
Hunter Perrin <hperrin@gmail.com>
2010-04-04 14:42:24 -0700

    More work on com_pinlock.

---
+++ pines/com_pinlock/views/xhtml-1.0/example.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Provide display editors for the fields of a Pines Form.
+ *
+ * @package Pines
+ * @subpackage com_pdf
+ * @license http://www.gnu.org/licenses/agpl-3.0.html
+ * @author Hunter Perrin <hunter@sciactive.com>
+ * @copyright Hunter Perrin
+ * @link http://sciactive.com/
+ */
+defined('P_RUN') or die('Direct access prohibited');
+$this->title = 'Action Succeeded';
+?>
+<p>If you're seeing this page, it means that you have successfully run the example action.</p>

c586bd4378700377fdef24a693f66c0286e196b4
Hunter Perrin <hperrin@gmail.com>
2010-04-08 15:54:22 -0700

    Work on the customer timer.

--- pines/com_customertimer/classes/com_customertimer_customer.php
+++ pines/com_customertimer/classes/com_customertimer_customer.php
@@ -19,11 +19,14 @@ defined('P_RUN') or die('Direct access prohibited');
  */
 class com_customertimer_customer extends com_customer_customer {
 	public function com_customertimer_login(&$floor, $station) {
-		//
+		if ($this->com_customertimer_is_logged_in($floor, $station))
+			return true;
+		
 	}
 
 	public function com_customertimer_logout(&$floor, $station) {
-		//
+		if (!$this->com_customertimer_is_logged_in($floor, $station))
+			return true;
 	}
 
 	public function com_customertimer_is_logged_in(&$floor, $station = null) {

dfd2fa4cf05f0d213f5b790b47d5a18663209545
Hunter Perrin <hperrin@gmail.com>
2010-04-25 00:28:09 -0700

    Began using new JS loader function.

--- pines/com_sales/views/xhtml-1.0/cash_drawer.php
+++ pines/com_sales/views/xhtml-1.0/cash_drawer.php
@@ -11,4 +11,8 @@
  */
 defined('P_RUN') or die('Direct access prohibited');
 ?>
-<script type="text/javascript" src="<?php echo $pines->config->rela_location; ?>components/com_sales/includes/cash_drawer.js"></script>
+<script type="text/javascript">
+	// <![CDATA[
+	pines.loadjs("<?php echo $pines->config->rela_location; ?>components/com_sales/includes/cash_drawer.js");
+	// ]]>
+</script>

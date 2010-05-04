1c2ac91b2bf087199875f1330659dd60f1fb276a
Hunter Perrin <hperrin@gmail.com>
2010-05-04 12:29:48 -0700

    Made a new service for icon themes. Created a component for the Tango icon theme.

---
+++ com_tangoicons/classes/com_tangoicons.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * com_tangoicons class.
+ *
+ * @package Pines
+ * @subpackage com_tangoicons
+ * @license http://www.gnu.org/licenses/agpl-3.0.html
+ * @author Hunter Perrin <hunter@sciactive.com>
+ * @copyright SciActive.com
+ * @link http://sciactive.com/
+ */
+defined('P_RUN') or die('Direct access prohibited');
+
+/**
+ * com_tangoicons main class.
+ *
+ * A Pines Icon theme using the Tango icon library.
+ *
+ * @package Pines
+ * @subpackage com_tangoicons
+ */
+class com_tangoicons extends component {
+	/**
+	 * Whether the Tango CSS has been loaded.
+	 * @access private
+	 * @var bool $css_loaded
+	 */
+	private $css_loaded = false;
+
+	/**
+	 * Load the icon set.
+	 *
+	 * This will place the required scripts into the document's head section.
+	 */
+	function load() {
+		if (!$this->css_loaded) {
+			$module = new module('com_tangoicons', 'tangoicons', 'head');
+			$this->css_loaded = true;
+		}
+	}
+}
+
+?>
---
+++ com_tangoicons/views/xhtml-1.0/tangoicons.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * A view to load the Tango icons.
+ *
+ * @package Pines
+ * @subpackage com_tangoicons
+ * @license http://www.gnu.org/licenses/agpl-3.0.html
+ * @author Hunter Perrin <hunter@sciactive.com>
+ * @copyright SciActive.com
+ * @link http://sciactive.com/
+ */
+defined('P_RUN') or die('Direct access prohibited');
+?>
+<script type="text/javascript">
+	// <![CDATA[
+	pines.loadcss("<?php echo $pines->config->rela_location; ?>components/com_tangoicons/includes/tango/icons.css");
+	// ]]>
+</script>

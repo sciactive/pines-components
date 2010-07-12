b41b4730eacf5481c9545a1992ce64b80a122f64
Zak Huber <zak@sciactive.com>
2010-07-12 16:14:17 -0700

    Fixed a problem with ability checking for cashcounts.

--- com_sales/actions/cashcount/savedeposit.php
+++ com_sales/actions/cashcount/savedeposit.php
@@ -12,7 +12,7 @@
 defined('P_RUN') or die('Direct access prohibited');
 
 
-if ( !gatekeeper('com_sales/auditcashcount') )
+if ( !gatekeeper('com_sales/depositcashcount') )
 	punt_user('You don\'t have necessary permission.', pines_url('com_sales', 'cashcount/list'));
 
 $cashcount = com_sales_cashcount::factory((int) $_REQUEST['id']);

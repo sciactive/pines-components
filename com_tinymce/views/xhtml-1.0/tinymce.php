2e8021acdd51c7bbee52ae2aab34fe4c2ab1da87
Hunter Perrin <hperrin@gmail.com>
2010-09-16 12:38:33 -0700

    Fixed bug with com_tinymce when using static URL.

--- com_tinymce/views/xhtml-1.0/tinymce.php
+++ com_tinymce/views/xhtml-1.0/tinymce.php
@@ -20,7 +20,7 @@ if (isset($pines->com_elfinder))
 	pines(function(){
 		$("textarea.peditor").tinymce({
 			// Location of TinyMCE script
-			script_url : '<?php echo htmlspecialchars($pines->config->location); ?>components/com_tinymce/includes/tiny_mce/tiny_mce.js',
+			script_url : '<?php echo htmlspecialchars($pines->config->rela_location); ?>components/com_tinymce/includes/tiny_mce/tiny_mce.js',
 			// General options
 			theme : "advanced",
 			plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
@@ -54,7 +54,7 @@ if (isset($pines->com_elfinder))
 		});
 		$("textarea.peditor-simple").tinymce({
 			// Location of TinyMCE script
-			script_url : '<?php echo htmlspecialchars($pines->config->location); ?>components/com_tinymce/includes/tiny_mce/tiny_mce.js',
+			script_url : '<?php echo htmlspecialchars($pines->config->rela_location); ?>components/com_tinymce/includes/tiny_mce/tiny_mce.js',
 			// General options
 			theme : "simple",
 			// Template's editor CSS

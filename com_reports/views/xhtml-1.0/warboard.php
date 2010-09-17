dc866905a17d891428e827446041613c559abced
Zak Huber <zak@sciactive.com>
2010-09-16 18:29:02 -0700

    Updates to the employee warboard. Terminated employees are no longer shown on the warboard.

--- com_reports/views/xhtml-1.0/warboard.php
+++ com_reports/views/xhtml-1.0/warboard.php
@@ -15,27 +15,27 @@ $this->title = htmlspecialchars($this->entity->company_name).' Warboard';
 <style type="text/css" >
 	/* <![CDATA[ */
 	#p_muid_warboard_table {
-		width: 100%;
-		text-align: center;
+		background-color: white;
+		color: black;
 		font-size: 8pt;
+		text-align: center;
 		white-space: nowrap;
+		width: 100%;
 	}
 	#p_muid_warboard_table td {
-		width: <?php echo 100/$this->entity->columns; ?>%;
 		vertical-align: text-top;
+		width: <?php echo 100 / $this->entity->columns; ?>%;
 	}
 	#p_muid_warboard_table .location, .important, .hq {
-		width: 100%;
-		border-spacing: 0;
 		border-collapse: collapse;
+		border-spacing: 0;
+		width: 100%;
 	}
 	#p_muid_warboard_table .location td, .important td, .hq td {
 		border: solid 1px;
 	}
-	
 	#p_muid_warboard_table .location .label {
 		background-color: beige;
-		color: black;
 	}
 	#p_muid_warboard_table .location .heading {
 		background-color: gainsboro;
@@ -43,14 +43,15 @@ $this->title = htmlspecialchars($this->entity->company_name).' Warboard';
 	}
 	#p_muid_warboard_table .important .label {
 		background-color: lightsteelblue;
-		color: black;
 	}
 	#p_muid_warboard_table .hq .label {
 		background-color: palegreen;
-		color: black;
 	}
 	#p_muid_warboard_table .empty {
 		background-color: #F1F1F1;
+	}
+	#p_muid_warboard_table .newhire {
+		background-color: #CCFFCC;
 	}
 	/* ]]> */
 </style>
@@ -63,65 +64,64 @@ $this->title = htmlspecialchars($this->entity->company_name).' Warboard';
 		$location_count[$cur_location->guid] = 0;
 		$employees = $cur_location->get_users();
 		$pines->entity_manager->sort($employees, 'hire_date');
-		if ($count > 0 && $count/$this->entity->columns == floor($count/$this->entity->columns)) { ?>
+		if ($count > 0 && ($count / $this->entity->columns) == floor($count / $this->entity->columns)) { ?>
 		</tr>
 		<tr>
 		<?php } ?>
 			<td>
-			<table class="location" id="location_<?php echo $cur_location->guid; ?>">
-				<tr class="label">
-					<td colspan="2">
-						<strong><?php
-						echo htmlspecialchars($cur_location->name);
-						if ($pines->config->com_reports->warboard_states)
-							echo ', ' . htmlspecialchars($cur_location->state);
-						?></strong>
-					</td>
-					<td><?php echo format_phone($cur_location->phone); ?></td>
-				</tr>
-				<tr class="heading">
-					<td colspan="3">District</td>
-				</tr>
-				<tr>
-					<td colspan="3"><?php echo isset($cur_location->parent->name) ? htmlspecialchars($cur_location->parent->name) : '-'; ?></td>
-				</tr>
-				<?php
-				foreach ($this->entity->positions as $cur_title) {
-					$location_count[$cur_location->guid]++;
-					$empty = true;
-				?>
-				<tr class="heading <?php echo strtolower(preg_replace('/ /', '_', $cur_location->guid.$cur_title)); ?>">
-					<td colspan="3"><?php echo htmlspecialchars($cur_title).$plural; ?></td>
-				</tr>
-				<?php
-				foreach ($employees as $key => &$cur_employee) {
-					if (!$cur_employee->employee) {
-						unset($cur_employee);
-					} elseif ($cur_employee->job_title == $cur_title) {
+				<table class="location" id="location_<?php echo $cur_location->guid; ?>">
+					<tr class="label">
+						<td colspan="2">
+							<strong><?php
+							echo htmlspecialchars($cur_location->name);
+							if ($pines->config->com_reports->warboard_states)
+								echo ', ' . htmlspecialchars($cur_location->state);
+							?></strong>
+						</td>
+						<td><?php echo format_phone($cur_location->phone); ?></td>
+					</tr>
+					<tr class="heading">
+						<td colspan="3">District</td>
+					</tr>
+					<tr>
+						<td colspan="3"><?php echo isset($cur_location->parent->name) ? htmlspecialchars($cur_location->parent->name) : '-'; ?></td>
+					</tr>
+					<?php
+					foreach ($this->entity->positions as $cur_title) {
+						$location_count[$cur_location->guid]++;
+						$empty = true;
+					?>
+					<tr class="heading <?php echo strtolower(str_replace(' ', '_', $cur_location->guid.$cur_title)); ?>">
+						<td colspan="3"><?php echo htmlspecialchars($cur_title).$plural; ?></td>
+					</tr>
+					<?php
+					foreach ($employees as $cur_employee) {
+						if (!$cur_employee->employee || $cur_employee->terminated || $cur_employee->job_title != $cur_title)
+							continue;
 						$location_count[$cur_location->guid]++;
 						$empty = false;
-				?>
-				<tr>
-					<td style="width: 25%;"><?php echo format_date($cur_employee->hire_date, 'custom', 'n/j/y'); ?></td>
-					<td style="width: 50%;"><?php echo htmlspecialchars($cur_employee->name); ?></td>
-					<td style="width: 25%;"><?php echo format_phone($cur_employee->phone); ?></td>
-				</tr>
-				<?php }
-				}
-				if ($empty) {
-					$location_count[$cur_location->guid]--; ?>
-				<script type="text/javascript">
-					// <![CDATA[
-					pines(function(){
-						$(".<?php echo strtolower(preg_replace('/ /', '_', $cur_location->guid.$cur_title)); ?>").hide();
-					});
-					// ]]>
-				</script>
-				<?php }
-				}
-				if ($location_count[$cur_location->guid] > $location_rows) $location_rows = $location_count[$cur_location->guid];
-				?>
-			</table>
+						?>
+					<tr <?php echo ($cur_employee->hire_date > strtotime('2 weeks ago')) ? 'class="newhire"' : ''; ?>>
+						<td style="width: 25%;"><?php echo format_date($cur_employee->hire_date, 'custom', 'n/j/y'); ?></td>
+						<td style="width: 50%;"><?php echo htmlspecialchars($cur_employee->name); ?></td>
+						<td style="width: 25%;"><?php echo format_phone($cur_employee->phone); ?></td>
+					</tr>
+						<?php
+					}
+					if ($empty) {
+						$location_count[$cur_location->guid]--; ?>
+					<script type="text/javascript">
+						// <![CDATA[
+						pines(function(){
+							$(".<?php echo strtolower(str_replace(' ', '_', $cur_location->guid.$cur_title)); ?>").hide();
+						});
+						// ]]>
+					</script>
+					<?php }
+					}
+					if ($location_count[$cur_location->guid] > $location_rows) $location_rows = $location_count[$cur_location->guid];
+					?>
+				</table>
 			</td>
 		<?php $count++; } ?>
 	</tr>
@@ -155,52 +155,45 @@ $this->title = htmlspecialchars($this->entity->company_name).' Warboard';
 			$pines->entity_manager->sort($employees, 'job_title');
 		?>
 		<td colspan="<?php echo floor(($this->entity->columns - 1)/count($this->entity->important)); ?>">
-		<table class="important" id="important_<?php echo $cur_important->guid; ?>">
-			<tr>
-				<td colspan="4" class="label"><strong><?php echo htmlspecialchars($cur_important->name); ?></strong></td>
-			</tr>
-			<?php
-			foreach ($employees as $key => &$cur_employee) {
-				if (!$cur_employee->employee) {
-					unset($cur_employee);
-				} else {
+			<table class="important" id="important_<?php echo $cur_important->guid; ?>">
+				<tr>
+					<td colspan="4" class="label"><strong><?php echo htmlspecialchars($cur_important->name); ?></strong></td>
+				</tr>
+				<?php
+				foreach ($employees as $cur_employee) {
+					if (!$cur_employee->employee || $cur_employee->terminated)
+						continue;
 					$important_count[$cur_important->guid]++;
-			?>
-			<tr>
-				<td><?php echo htmlspecialchars($cur_employee->name); ?></td>
-				<td><?php echo htmlspecialchars($cur_employee->job_title); ?></td>
-				<td><?php echo format_phone($cur_employee->phone); ?></td>
-				<td><?php echo !empty($cur_employee->phone_ext) ? 'ext '.$cur_employee->phone_ext : ''; ?></td>
-			</tr>
-			<?php } } ?>
-		</table>
+				?>
+				<tr>
+					<td><?php echo htmlspecialchars($cur_employee->name); ?></td>
+					<td><?php echo htmlspecialchars($cur_employee->job_title); ?></td>
+					<td><?php echo format_phone($cur_employee->phone); ?></td>
+					<td><?php echo !empty($cur_employee->phone_ext) ? 'ext '.$cur_employee->phone_ext : ''; ?></td>
+				</tr>
+				<?php } ?>
+			</table>
 		</td>
 		<?php if ($important_count[$cur_important->guid] > $important_rows) $important_rows = $important_count[$cur_important->guid]; } ?>
 	</tr>
 </table>
+<script type="text/javascript">
+	// <![CDATA[
+	pines(function(){
 <?php
 foreach ($this->entity->locations as $cur_location) {
 	$add_rows = $location_rows - $location_count[$cur_location->guid];
 	for ($i=0; $i < $add_rows; $i++) { ?>
-<script type="text/javascript">
-	// <![CDATA[
-	pines(function(){
 		$("#location_<?php echo $cur_location->guid; ?>").append('<tr class="empty"><td style="width: 25%;">&nbsp;</td><td style="width: 50%;">&nbsp;</td><td style="width: 25%;">&nbsp;</td></tr>');
-	});
-	// ]]>
-</script>
 <?php
 	}
 }
 foreach ($this->entity->important as $cur_important) {
 	$add_rows = $important_rows - $important_count[$cur_important->guid];
-	for ($i=0; $i < $add_rows; $i++) {
-?>
-<script type="text/javascript">
-	// <![CDATA[
-	pines(function(){
+	for ($i=0; $i < $add_rows; $i++) { ?>
 		$("#important_<?php echo $cur_important->guid; ?>").append('<tr class="empty"><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>');
+<?php } } ?>
+		return;
 	});
 	// ]]>
 </script>
-<?php } } ?>

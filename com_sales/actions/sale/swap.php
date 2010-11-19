5b9dd0ea0f0658c2ae5353cb4b46ef76ffe1977c
Zak Huber <zak@sciactive.com>
2010-11-19 13:42:22 -0800

    Forgot one file for the swap system.

---
+++ com_sales/actions/sale/swap.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Swap an item in a sale.
+ *
+ * @package Pines
+ * @subpackage com_sales
+ * @license http://www.gnu.org/licenses/agpl-3.0.html
+ * @author Zak Huber <zak@sciactive.com>
+ * @copyright SciActive.com
+ * @link http://sciactive.com/
+ */
+defined('P_RUN') or die('Direct access prohibited');
+
+if ( !gatekeeper('com_sales/swapsale') )
+	punt_user(null, pines_url('com_sales', 'sale/swap', array('id' => $_REQUEST['id'])));
+
+$entity = com_sales_sale::factory((int) $_REQUEST['id']);
+
+if (!isset($entity->guid)) {
+	pines_notice('The given ID could not be found.');
+	redirect(pines_url('com_sales', 'sale/list'));
+	return;
+}
+$swap_item = explode(':', $_REQUEST['swap_item']);
+$sku = $swap_item[0];
+$old_serial = $swap_item[1];
+$new_serial = $_REQUEST['new_serial'];
+if (empty($old_serial) || empty($new_serial)){
+	pines_notice('Only serialized items may be swapped.');
+	redirect(pines_url('com_sales', 'sale/list'));
+	return;
+}
+if ($entity->swap($sku, $old_serial, $new_serial) && $entity->save()) {
+	pines_notice("Item [$old_serial] has been swapped with [$new_serial].");
+} else {
+	pines_notice('The items could not be swapped.');
+}
+
+redirect(pines_url('com_sales', 'sale/list'));
+
+?>

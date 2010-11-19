e7a197fb0d8d695f318250206715df63b6616e27
Zak Huber <zak@sciactive.com>
2010-11-19 15:39:01 -0800

    Fixed a small bug with the swap system.

--- com_sales/views/xhtml-1.0/sale/list.php
+++ com_sales/views/xhtml-1.0/sale/list.php
@@ -53,7 +53,7 @@ if (isset($_SESSION['user']) && is_array($_SESSION['user']->pgrid_saved_states))
 				{type: 'button', text: 'Receipt', extra_class: 'picon picon-document-print-preview', double_click: true, url: '<?php echo addslashes(pines_url('com_sales', 'sale/receipt', array('id' => '__title__'))); ?>'},
 				<?php if (gatekeeper('com_sales/newreturnwsale')) { ?>
 				{type: 'button', text: 'Return', extra_class: 'picon picon-edit-undo', url: '<?php echo addslashes(pines_url('com_sales', 'sale/return', array('id' => '__title__'))); ?>'},
-				<?php } if (gatekeeper('com_sales/swap')) { ?>
+				<?php } if (gatekeeper('com_sales/swapsale')) { ?>
 				{type: 'button', text: 'Swap', extra_class: 'picon picon-document-swap', click: function(e, row){
 					sale_grid.swap_form($(row).attr("title"));
 				}},

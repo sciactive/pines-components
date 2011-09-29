37b63e2543140e20f54b47bd43fe3e6046d52164
Hunter Perrin <hperrin@gmail.com>
2011-09-28 17:01:34 -0700

    Changed URL for public entering.

--- com_raffle/actions/raffle/enter.php
+++ com_raffle/actions/raffle/enter.php
@@ -24,7 +24,7 @@ if ($entity->complete) {
 $phone = preg_replace('/\D/', '', $_REQUEST['phone']);
 if (empty($_REQUEST['first_name']) || empty($_REQUEST['last_name']) || empty($_REQUEST['email']) || empty($phone)) {
 	pines_notice('Please provide all requested information.');
-	pines_redirect(pines_url('com_raffle', 'raffle', array('id' => $entity->guid)));
+	pines_redirect(pines_url('com_raffle', 'enter', array('id' => $entity->guid)));
 	return;
 }
 
@@ -32,7 +32,7 @@ $email_check = strtolower($_REQUEST['email']);
 foreach ($entity->public_contestants as $cur_contestant) {
 	if ($email_check == strtolower($cur_contestant['email'])) {
 		pines_notice('You have been entered into the raffle.');
-		pines_redirect($entity->back_to_form ? pines_url('com_raffle', 'raffle', array('id' => $entity->guid)) : pines_url());
+		pines_redirect($entity->back_to_form ? pines_url('com_raffle', 'enter', array('id' => $entity->guid)) : pines_url());
 		return;
 	}
 }
@@ -46,10 +46,10 @@ $entity->public_contestants[] = array(
 
 if ($entity->save()) {
 	pines_notice('You have been entered into the raffle.');
-	pines_redirect($entity->back_to_form ? pines_url('com_raffle', 'raffle', array('id' => $entity->guid)) : pines_url());
+	pines_redirect($entity->back_to_form ? pines_url('com_raffle', 'enter', array('id' => $entity->guid)) : pines_url());
 } else {
 	pines_error('Your entry could not be saved. Please try again.');
-	pines_redirect(pines_url('com_raffle', 'raffle', array('id' => $entity->guid)));
+	pines_redirect(pines_url('com_raffle', 'enter', array('id' => $entity->guid)));
 }
 
 ?>

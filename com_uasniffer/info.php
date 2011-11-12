4d2db2f73ee3bd9b831eceb81a901d797a41e661
Hunter Perrin <hperrin@gmail.com>
2011-11-11 19:34:02 -0800

    Removed incorrect service provider listing in com_uasniffer.

--- com_uasniffer/info.php
+++ com_uasniffer/info.php
@@ -18,7 +18,6 @@ return array(
 	'version' => '1.0.0',
 	'license' => 'http://www.gnu.org/licenses/agpl-3.0.html',
 	'website' => 'http://www.sciactive.com',
-	'services' => array('user_manager'),
 	'short_description' => 'Provides browser specific options',
 	'description' => 'Uses the client\'s user agent string to determine what browser they\'re using. You can provide browser specific configuration, including configuration for mobile browsers.',
 	'depend' => array(

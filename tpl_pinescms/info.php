3ad61259631a1d8bf5ebbc2c1f8db15dac74ce6f
Hunter Perrin <hperrin@gmail.com>
2012-02-01 19:06:30 -0800

    Some changes to Inuit CSS. Incremented versions on modified templates.

--- tpl_pinescms/info.php
+++ tpl_pinescms/info.php
@@ -15,7 +15,7 @@ defined('P_RUN') or die('Direct access prohibited');
 return array(
 	'name' => 'Pines CMS Template',
 	'author' => 'SciActive',
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'license' => 'http://www.gnu.org/licenses/agpl-3.0.html',
 	'website' => 'http://www.sciactive.com',
 	'services' => array('template'),

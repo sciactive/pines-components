5210e8fa7cb0bfa49dbb547abdcf776f88689005
Hunter Perrin <hperrin@gmail.com>
2012-02-14 17:48:56 -0800

    Updated Pines Notify to latest version.

--- com_pnotify/info.php
+++ com_pnotify/info.php
@@ -15,7 +15,7 @@ defined('P_RUN') or die('Direct access prohibited');
 return array(
 	'name' => 'Pines Notify',
 	'author' => 'SciActive',
-	'version' => '1.0.0',
+	'version' => '1.1.0',
 	'license' => 'http://www.gnu.org/licenses/agpl-3.0.html',
 	'website' => 'http://www.sciactive.com',
 	'short_description' => 'Pines Notify jQuery plugin',

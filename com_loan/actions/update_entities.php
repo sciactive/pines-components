ff82429b7fb6a3e4d35347af5d552f8a920b03fc
Angela Murrell <angela@sciactive.com>
2012-05-17 11:13:51 -0700

    Update entities script for updating the loan first payment due and scheduled due dates to midnight.

---
+++ com_loan/actions/update_entities.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Update loan entities.
+ *
+ * @package com_loan
+ * @license none
+ * @author Hunter Perrin <hunter@sciactive.com>
+ * @copyright Smart Industries, LLC
+ * @link http://smart108.com/
+ */
+defined('P_RUN') or die('Direct access prohibited');
+
+if ( !gatekeeper('system/all') )
+    punt_user('You don\'t have necessary permission.', pines_url());
+
+$module = new module('system', 'null', 'content');
+$module->title = 'Loan Entity Update';
+
+$errors = array();
+$count = $nochange = 0;
+// Grab entities and update.
+$entities = $pines->entity_manager->get_entities(
+		array('class' => com_loan_loan),
+		array('&',
+			'tag' => array('com_loan', 'loan'),
+		)
+	);
+
+foreach ($entities as &$cur_entity) {
+	$changed = false;
+	if (format_date($cur_entity->first_payment_date, 'custom', 'H:i') != '00:00') {
+		$cur_entity->first_payment_date = strtotime('00:00:00', $cur_entity->first_payment_date);
+		$changed = true;
+	}
+	foreach ($cur_entity->schedule as &$schedule) {
+		if (format_date($schedule['scheduled_date_expected'], 'custom', 'H:i') != '00:00') {
+			$schedule['scheduled_date_expected'] = strtotime('00:00:00', $schedule['scheduled_date_expected']);
+			$changed = true;
+		}
+	}
+	unset($schedule);
+	if ($changed) {
+		if ($cur_entity->save())
+			$count++;
+		else
+			$errors[] = $cur_entity->guid;
+	} else {
+		$nochange++;
+	}
+}
+unset($cur_entity);
+
+$module->content("Updated $count entities. Found $nochange entities that didn't need to be updated.");
+if ($errors)
+	$module->content('<br />Could not update the entities: '.implode(', ', $errors));
+
+?>

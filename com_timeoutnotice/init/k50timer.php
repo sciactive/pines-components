3a43d3b0c89ad9d41266b153eeaff89bda5d164d
Hunter Perrin <hperrin@gmail.com>
2012-06-01 16:11:26 -0700

    Prevented timeout notice from resetting the timer on XMPP communications.

--- com_timeoutnotice/init/k50timer.php
+++ com_timeoutnotice/init/k50timer.php
@@ -13,7 +13,10 @@ defined('P_RUN') or die('Direct access prohibited');
 
 if ( gatekeeper() ) {
 	pines_session('write');
-	if ($pines->request_component != 'com_timeoutnotice' || $pines->request_action != 'check')
+	if (
+			($pines->request_component != 'com_timeoutnotice' || $pines->request_action != 'check') &&
+			($pines->request_component != 'com_messenger' || $pines->request_action != 'xmpp_proxy')
+		)
 		$_SESSION['com_timeoutnotice__last_access'] = time();
 	// This stores any custom config value.
 	$_SESSION['com_timeoutnotice__timeout'] = $pines->config->com_timeoutnotice->timeout;

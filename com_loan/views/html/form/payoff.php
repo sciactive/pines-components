5f81f8ec176b846aeff6c47d75fff50c6c1d39c4
Hunter Perrin <hperrin@gmail.com>
2012-06-20 12:09:00 -0700

    Documentation fixes.

--- com_loan/views/html/form/payoff.php
+++ com_loan/views/html/form/payoff.php
@@ -2,7 +2,7 @@
 /**
  * Display a form to pay off the loan.
  *
- * @package Components\smartflights
+ * @package Components\loan
  * @license http://www.gnu.org/licenses/agpl-3.0.html
  * @author Angela Murrell <angela@sciactive.com>
  * @copyright SciActive.com

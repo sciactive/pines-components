716e88d169a33414452ad973c5624ecb7cd8bd1d
Hunter Perrin <hperrin@gmail.com>
2012-07-20 13:37:34 -0700

    Updated to new version of Pines Form.

--- com_pform/views/html/pform.php
+++ com_pform/views/html/pform.php
@@ -12,29 +12,15 @@
 defined('P_RUN') or die('Direct access prohibited');
 ?>
 <script type="text/javascript">
-pines.loadcss("<?php echo htmlspecialchars($pines->config->location); ?>components/com_pform/includes/pform.css");
+pines.loadcss("<?php echo htmlspecialchars($pines->config->location); ?>components/com_pform/includes/<?php echo $pines->config->debug_mode ? 'pform.css' : 'pform.min.css'; ?>");
 <?php if ($pines->depend->check('component', 'com_bootstrap')) { ?>
-pines.loadcss("<?php echo htmlspecialchars($pines->config->location); ?>components/com_pform/includes/pform-bootstrap.css");
+pines.loadcss("<?php echo htmlspecialchars($pines->config->location); ?>components/com_pform/includes/<?php echo $pines->config->debug_mode ? 'pform-bootstrap.css' : 'pform-bootstrap.min.css'; ?>");
 <?php } ?></script>
 <!--[if lt IE 8]>
 <script type="text/javascript">
-pines.loadcss("<?php echo htmlspecialchars($pines->config->location); ?>components/com_pform/includes/pform-ie-lt-8.css");</script>
+pines.loadcss("<?php echo htmlspecialchars($pines->config->location); ?>components/com_pform/includes/<?php echo $pines->config->debug_mode ? 'pform-ie-lt-8.css' : 'pform-ie-lt-8.min.css'; ?>");</script>
 <![endif]-->
 <!--[if lt IE 6]>
 <script type="text/javascript">
-pines.loadcss("<?php echo htmlspecialchars($pines->config->location); ?>components/com_pform/includes/pform-ie-lt-6.css");</script>
+pines.loadcss("<?php echo htmlspecialchars($pines->config->location); ?>components/com_pform/includes/<?php echo $pines->config->debug_mode ? 'pform-ie-lt-6.css' : 'pform-ie-lt-6.min.css'; ?>");</script>
 <![endif]-->
-<style type="text/css">
-.pf-form .pf-element .pf-label, .pf-form .pf-element .pf-note {
-width: 180px;
-}
-.pf-form .pf-element .pf-group {
-margin-left: 180px;
-}
-.pf-form .pf-element .pf-field.pf-full-width {
-margin-left: 185px;
-}
-.pf-form .pf-buttons {
-padding-left: 165px;
-}
-</style>

2bebdf4d2d8fd4b7e4d7538f5789773f18a8831a
amurrell <angela@sciactive.com>
2013-06-21 11:41:36 -0700

    For com_elastislide, consolidated all the elastislide JS files into one minified version. Added a configuration option to it so that the minified combined file can be used, or not.

---
+++ com_elastislide/defaults.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * com_elastislide's information.
+ *
+ * @package Components\elastislide
+ * @license http://www.gnu.org/licenses/agpl-3.0.html
+ * @author Angela Murrell <angela@sciactive.com>
+ * @copyright SciActive.com
+ * @link http://sciactive.com/
+ */
+/* @var $pines pines */
+defined('P_RUN') or die('Direct access prohibited');
+
+return array(
+	array(
+		'name' => 'combine_js',
+		'cname' => 'Use Combined JS',
+		'description' => 'Use a minified, combined JS file instead of multiple.',
+		'value' => false,
+		'peruser' => true,
+	)
+);
+
+?>
--- com_elastislide/views/html/elastislide.php
+++ com_elastislide/views/html/elastislide.php
@@ -11,9 +11,16 @@
 /* @var $pines pines *//* @var $this module */
 defined('P_RUN') or die('Direct access prohibited');
 ?>
+<?php if ($pines->config->com_elastislide->combine_js) { ?>
+<script type="text/javascript">
+	pines.loadcss("<?php echo htmlspecialchars($pines->config->location); ?>components/com_elastislide/includes/jquery.elastislide.css");
+	pines.loadjs("<?php echo htmlspecialchars($pines->config->location); ?>components/com_elastislide/includes/jquery.complete.elastislide.js");
+</script>
+<?php } else { ?>
 <script type="text/javascript">
 	pines.loadcss("<?php echo htmlspecialchars($pines->config->location); ?>components/com_elastislide/includes/jquery.elastislide.css");
 	pines.loadjs("<?php echo htmlspecialchars($pines->config->location); ?>components/com_elastislide/includes/modernizr.custom.17475.js");
 	pines.loadjs("<?php echo htmlspecialchars($pines->config->location); ?>components/com_elastislide/includes/jquerypp.custom.js");
 	pines.loadjs("<?php echo htmlspecialchars($pines->config->location); ?>components/com_elastislide/includes/jquery.elastislide.js");
 </script>
+<?php } ?>

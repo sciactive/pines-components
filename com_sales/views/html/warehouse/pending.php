1d45cf9ce5f772a55369ccc120ab2b1e09f8abeb
mohammed-ahmed <mohammedsadikahmed@gmail.com>
2014-02-25 14:59:56 -0800

    Modified hours_clocked in com_reports and added an Export to Google Drive button to the view pending in com_sales

--- com_sales/views/html/warehouse/pending.php
+++ com_sales/views/html/warehouse/pending.php
@@ -30,6 +30,14 @@ if ($this->all_time) {
 }
 $pines->com_pgrid->load();
 $pines->com_jstree->load();
+$google_drive = false;
+if (isset($pines->com_googledrive)) {
+    $pines->com_googledrive->export_to_drive('csv');
+    $google_drive = true;
+} else {
+    pines_log("Google Drive is not installed", 'notice');
+}
+
 if (isset($_SESSION['user']) && is_array($_SESSION['user']->pgrid_saved_states))
 	$this->pgrid_state = (object) json_decode($_SESSION['user']->pgrid_saved_states['com_sales/warehouse/pending']);
 ?>
@@ -163,7 +171,18 @@ if (isset($_SESSION['user']) && is_array($_SESSION['user']->pgrid_saved_states))
 						filename: 'pending warehouse orders',
 						content: rows
 					});
-				}}
+				}},
+                                <?php // Need to check if Google Drive is installed
+                                    if ($google_drive && !empty($pines->config->com_googledrive->client_id)) { ?>
+                                        {type: 'button', title: 'Export to Google Drive', extra_class: 'picon drive-icon', multi_select: true, pass_csv_with_headers: true, click: function(e, rows){
+                                        setRows(rows);
+                                        checkAuth();
+                                    }},
+                                    <?php } elseif ($google_drive && empty($pines->config->com_googledrive->client_id)) { ?>
+                                        {type: 'button', title: 'Export to Google Drive', extra_class: 'picon drive-icon', multi_select: true, pass_csv_with_headers: true, click: function(e, rows){
+                                        alert('You need to set the CLIENT ID before you can export to Google Drive');
+                                    }},
+                                    <?php } ?>
 			],
 			pgrid_sort_col: 1,
 			pgrid_sort_ord: 'asc',

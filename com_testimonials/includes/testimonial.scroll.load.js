f782aca6d0dfeca7fd184ac86b5a96f8d3ee4c2f
amurrell <angela@sciactive.com>
2014-04-07 12:14:03 -0700

    On com_testimonials, altered the scroll load js to start the loading at a higher point above the element.

--- com_testimonials/includes/testimonial.scroll.load.js
+++ com_testimonials/includes/testimonial.scroll.load.js
@@ -4,7 +4,7 @@ pines(function(){
 	var scroll_handler = function(){
 		page_testimonials.each(function(){
 			var testimonial = $(this);
-			var test_offset = testimonial.offset().top - 100;
+			var test_offset = testimonial.offset().top - 300;
 			var scroll_point = $(window).scrollTop();
 			var scroll_point_hit = (scroll_point + $(window).height() == $(document).height()) ? true : (test_offset < scroll_point);
 			if (!testimonial.hasClass('loaded') && scroll_point_hit) {

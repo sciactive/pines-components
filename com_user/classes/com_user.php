ca3f0ba4710433e08e388ac15ad65e44b16db94c
amurrell <angela@sciactive.com>
2014-05-15 12:18:32 -0700

    On com_user, during filling the session, the config init from com_configure has been included to avoid any problems with loading configurations.

--- com_user/classes/com_user.php
+++ com_user/classes/com_user.php
@@ -290,8 +290,9 @@ class com_user extends component implements user_manager_interface {
 		// the custom config in com_configure i14 will never happen
 		// and because of caching, it will not get properly loaded like it should
 		// here.
-		if (($tmp_user->sys_config || $tmp_user->com_config) && $pines->depend->check('component', 'com_configure'))
-			$pines->configurator->load_per_user_array($tmp_user->sys_config, $tmp_user->com_config);
+		if ($pines->depend->check('component', 'com_configure')) {
+			include('components/com_configure/init/i14custom_config.php');
+		}
 		pines_session('close');
 	}
 
